85ae61b0ebaa92cffa413831c763cb8fe7a0df93
andyshen2 <andyshen96@gmail.com>
2020-08-29 21:41:50 +1200

    Change styling and clean up code

--- app/src/main/java/com/example/nzspeciesclassification/MainActivity.java
+++ app/src/main/java/com/example/nzspeciesclassification/MainActivity.java
@@ -1,59 +1,27 @@
 package com.example.nzspeciesclassification;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.FileProvider;
 import android.Manifest;
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Matrix;
-import android.media.ExifInterface;
-import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
-import android.os.Environment;
-import android.provider.MediaStore;
-import android.util.Log;
-import android.util.StateSet;
 import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.ImageButton;
-import android.widget.ImageView;
-import android.widget.ListView;
-import com.example.nzspeciesclassification.env.Logger;
-
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.List;
-import com.example.nzspeciesclassification.tflite.Classifier;
-import com.example.nzspeciesclassification.tflite.Classifier.Device;
-import com.example.nzspeciesclassification.tflite.Classifier.Model;
-import com.example.nzspeciesclassification.tflite.Classifier.Recognition;
-import static android.os.Environment.getExternalStoragePublicDirectory;
-import static android.os.Environment.getExternalStorageDirectory;
 
 public class MainActivity extends AppCompatActivity {
-    private static final Logger LOGGER = new Logger();
     ImageButton cameraButton;
     ImageButton fileButton;
-    Button btnChoosePic;
-    ImageView imageView;
-    ListView predictions;
-    String pathToFile;
-    private Classifier classifier;
-    private Model model = Model.FLOAT;
-    private Device device = Device.CPU;
-    public static final int PICK_IMAGE = 2;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
 
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+
         setContentView(R.layout.activity_main);
 
         cameraButton = findViewById(R.id.cameraButton);
@@ -61,211 +29,32 @@ public class MainActivity extends AppCompatActivity {
 
 
         if(Build.VERSION.SDK_INT >= 23){
-            System.out.println("permissions");
-
             requestPermissions(new String[]{Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE}, 2);
-
-
         }
-
 
         fileButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                try{
-                    pickFromGallery();
 
-                }catch (Exception e){
-
-                }
+                Intent intent = new Intent(MainActivity.this, ResultsActivity.class);
+                boolean mainScreen = false;
+                intent.putExtra("mainScreen", mainScreen);
+                startActivity(intent);
             }
         });
+
         cameraButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                try{
-                    dispatchPictureTakenAction();
-
-                }catch (Exception e){
-
-                }
+                Intent intent = new Intent(MainActivity.this, ResultsActivity.class);
+                boolean mainScreen = true;
+                intent.putExtra("mainScreen", mainScreen);
+                startActivity(intent);
             }
         });
 
-
-        imageView = findViewById(R.id.imageView);
     }
 
 
 
-    public static Bitmap rotateImage(Bitmap source, float angle) {
-        Matrix matrix = new Matrix();
-        matrix.postRotate(angle);
-        return Bitmap.createBitmap(source, 0, 0, source.getWidth(), source.getHeight(),
-                matrix, true);
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        try {
-            LOGGER.d(
-                    "Creating classifier (model=%s, device=%s, numThreads=%d)", model, device, 4);
-            classifier = Classifier.create(this, model, device, 4);
-        } catch (IOException e) {
-            LOGGER.e(e, "Failed to create classifier.");
-        }
-
-        if(resultCode == RESULT_OK){
-            if(requestCode == 1){
-                File file = new File(pathToFile);
-                Bitmap bitmap = BitmapFactory.decodeFile(pathToFile);
-                try {
-                    ExifInterface ei = new ExifInterface(pathToFile);
-                    int orientation = ei.getAttributeInt(ExifInterface.TAG_ORIENTATION,
-                            ExifInterface.ORIENTATION_UNDEFINED);
-
-                    Bitmap rotatedBitmap = null;
-                    switch(orientation) {
-
-                        case ExifInterface.ORIENTATION_ROTATE_90:
-                            rotatedBitmap = rotateImage(bitmap, 90);
-                            break;
-
-                        case ExifInterface.ORIENTATION_ROTATE_180:
-                            rotatedBitmap = rotateImage(bitmap, 180);
-                            break;
-
-                        case ExifInterface.ORIENTATION_ROTATE_270:
-                            rotatedBitmap = rotateImage(bitmap, 270);
-                            break;
-
-                        case ExifInterface.ORIENTATION_NORMAL:
-                        default:
-                            rotatedBitmap = bitmap;
-                    }
-
-
-                    imageView.setImageBitmap(rotatedBitmap);
-                    Bitmap scaled = Bitmap.createScaledBitmap(rotatedBitmap, classifier.getImageSizeX(),classifier.getImageSizeY(), false);
-
-
-                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2) {
-                        final List<Recognition> results = classifier.recognizeImage(scaled.copy(Bitmap.Config.ARGB_8888,false));
-                        System.out.print(results + " RESULTS");
-                        ArrayAdapter adapter = new ArrayAdapter<Recognition>(this,
-                                android.R.layout.simple_list_item_1,
-                                results);
-                        int listLength = results.size();
-
-                        for( int i = 0 ; i < listLength; i++){
-                            String[]splitNames = results.get(i).toString().split("_");
-                            System.out.println(splitNames[0]);
-
-//                            System.out.println(results.get(i));
-
-                        }
-
-                        predictions = findViewById(R.id.predictions);
-                        predictions.setAdapter(adapter);
-
-                    }
-//                    ByteArrayOutputStream _bs = new ByteArrayOutputStream();
-//                    rotatedBitmap.compress(Bitmap.CompressFormat.PNG, 60, _bs);
-//                    Intent k = new Intent(MainActivity.this, ResultsActivity.class);
-//                    k.putExtra("image", _bs.toByteArray());
-//                    System.out.println("NOOO GOOO???");
-//                    startActivity(k);
-
-
-                }catch(Exception e){
-                    System.err.println(e);
-
-                }
-            }else if (requestCode == PICK_IMAGE){
-                Uri uri = data.getData();
-
-                try {
-                    Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
-                    ImageView imageView = findViewById(R.id.imageView);
-                    imageView.setImageBitmap(bitmap);
-                    Bitmap scaled = Bitmap.createScaledBitmap(bitmap, classifier.getImageSizeX(),classifier.getImageSizeY(), false);
-                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2) {
-                        final List<Recognition> results = classifier.recognizeImage(scaled.copy(Bitmap.Config.ARGB_8888,false));
-                        ArrayAdapter adapter = new ArrayAdapter<Recognition>(this,
-                                android.R.layout.simple_list_item_1,
-                                results);
-                        predictions = (ListView) findViewById(R.id.predictions);
-                        predictions.setAdapter(adapter);
-
-                        System.out.println(results);
-                    }
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-
-
-
-            }
-        }
-    }
-    private void classifyImage(){
-        //TODO: Will have a button that says classify and will trigger this method
-
-    }
-    private void pickFromGallery() throws IOException{
-        Intent intent = new Intent();
-        intent.setType("image/*");
-        intent.setAction(Intent.ACTION_GET_CONTENT);
-        File photoFile = null;
-
-        photoFile = createImageFile();
-        startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE);
-    }
-    String currentPhotoPath;
-
-    private File createImageFile() throws IOException {
-        // Create an image file name
-        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-        String imageFileName = "JPEG_" + timeStamp + "_";
-        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
-        File image = File.createTempFile(
-                imageFileName,  /* prefix */
-                ".jpg",         /* suffix */
-                storageDir      /* directory */
-        );
-
-        // Save a file: path for use with ACTION_VIEW intents
-        currentPhotoPath = image.getAbsolutePath();
-        return image;
-    }
-    private void dispatchPictureTakenAction() throws IOException{
-        Intent takePic = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        System.out.println("hrererere");
-        if(takePic.resolveActivity(getPackageManager()) != null){
-            File photoFile = null;
-
-            photoFile = createImageFile();
-            if(photoFile != null){
-                pathToFile = photoFile.getAbsolutePath();
-                Uri photoURI = FileProvider.getUriForFile(MainActivity.this, "com.example.nzspeciesclassification.fileprovider", photoFile);
-                takePic.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
-                startActivityForResult(takePic, 1);
-            }
-        }
-    }
-
-//    private File createPhotoFile() {
-//        String name = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-//        File storageDir = getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
-//        File image = null;
-//        try {
-//            image = File.createTempFile(name, ".jpg", storageDir);
-//        } catch (IOException e) {
-//            Log.d("mylog", "Error: " + e.toString());
-//        }
-//        return image;
-//
-//
-//    }
 }
--- app/src/main/java/com/example/nzspeciesclassification/ResultsActivity.java
+++ app/src/main/java/com/example/nzspeciesclassification/ResultsActivity.java
@@ -1,24 +1,237 @@
 package com.example.nzspeciesclassification;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.media.ExifInterface;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.ListView;
+
+import com.example.nzspeciesclassification.tflite.Classifier;
+import com.example.nzspeciesclassification.env.Logger;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 
 public class ResultsActivity extends AppCompatActivity {
+    ImageButton cameraButton;
+    ImageButton fileButton;
+    private Classifier classifier;
+    private Classifier.Model model = Classifier.Model.FLOAT;
+    private Classifier.Device device = Classifier.Device.CPU;
+    public static final int PICK_IMAGE = 2;
+    ListView predictions;
 
-    Intent intent = getIntent();
-    Bitmap bitmap = (Bitmap) intent.getParcelableExtra("image");
     ImageView imageView;
+    String pathToFile;
+    private static final Logger LOGGER = new Logger();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-                
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_results);
+
+
+        Intent intent = getIntent();
+        Bitmap bitmap = (Bitmap) intent.getParcelableExtra("BitmapImage");
+        Bundle bundle = getIntent().getExtras();
+        boolean mainScreen = bundle.getBoolean("mainScreen");
+        if(mainScreen) {
+            try {
+                dispatchPictureTakenAction();
+            } catch (Exception e) {
+                System.err.println(e);
+            }
+        }else{
+            try{
+                pickFromGallery();
+
+            }catch (Exception e){
+                System.err.println(e);
+            }
+        }
+
+        cameraButton = findViewById(R.id.cameraButton);
+        fileButton = findViewById(R.id.fileButton);
+
+        cameraButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try{
+                    dispatchPictureTakenAction();
+                }catch (Exception e){
+                    System.err.println(e);
+                }
+            }
+        });
+
+        fileButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try{
+                    pickFromGallery();
+
+                }catch (Exception e){
+                    System.err.println(e);
+                }
+            }
+        });
+
         imageView = findViewById(R.id.imageView);
-//        imageView.setImageBitmap(bitmap);
+
+    }
+    String currentPhotoPath;
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+    private void pickFromGallery() throws IOException{
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+
+        createImageFile();
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE);
+    }
+    private void dispatchPictureTakenAction() throws IOException {
+        Intent takePic = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        if(takePic.resolveActivity(getPackageManager()) != null){
+            File photoFile = null;
+
+            photoFile = createImageFile();
+            if(photoFile != null){
+                pathToFile = photoFile.getAbsolutePath();
+                Uri photoURI = FileProvider.getUriForFile(ResultsActivity.this, "com.example.nzspeciesclassification.fileprovider", photoFile);
+                takePic.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePic, 1);
+            }
+        }
+    }
+    public static Bitmap rotateImage(Bitmap source, float angle) {
+        Matrix matrix = new Matrix();
+        matrix.postRotate(angle);
+        return Bitmap.createBitmap(source, 0, 0, source.getWidth(), source.getHeight(),
+                matrix, true);
+    }
+
+    private void classifyImage(Bitmap bitmap){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2) {
+            final List<Classifier.Recognition> results = classifier.recognizeImage(bitmap.copy(Bitmap.Config.ARGB_8888,false));
+            int listLength = results.size();
+            List<String> speciesName = new ArrayList<>();
+            for( int i = 0 ; i < listLength; i++){
+                String[]splitNames = results.get(i).toString().split("_");
+                String [] species = splitNames[0].split(" ", 2);
+                String [] confidence = splitNames[1].split(" ");
+                speciesName.add(species[1] + " " + confidence[confidence.length-1]);
+            }
+            ArrayAdapter adapter = new ArrayAdapter<String>(this,
+                    android.R.layout.simple_list_item_1,
+                    speciesName);
+            predictions = findViewById(R.id.predictions);
+            predictions.setAdapter(adapter);
+        }
+
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        try {
+            LOGGER.d(
+                    "Creating classifier (model=%s, device=%s, numThreads=%d)", model, device, 4);
+            classifier = Classifier.create(this, model, device, 4);
+        } catch (IOException e) {
+            LOGGER.e(e, "Failed to create classifier.");
+        }
+
+        if(resultCode == RESULT_OK){
+            if(requestCode == 1){
+                File file = new File(pathToFile);
+                Bitmap bitmap = BitmapFactory.decodeFile(pathToFile);
+                try {
+                    ExifInterface ei = new ExifInterface(pathToFile);
+                    int orientation = ei.getAttributeInt(ExifInterface.TAG_ORIENTATION,
+                            ExifInterface.ORIENTATION_UNDEFINED);
+
+                    Bitmap rotatedBitmap = null;
+                    switch(orientation) {
+
+                        case ExifInterface.ORIENTATION_ROTATE_90:
+                            rotatedBitmap = rotateImage(bitmap, 90);
+                            break;
+
+                        case ExifInterface.ORIENTATION_ROTATE_180:
+                            rotatedBitmap = rotateImage(bitmap, 180);
+                            break;
+
+                        case ExifInterface.ORIENTATION_ROTATE_270:
+                            rotatedBitmap = rotateImage(bitmap, 270);
+                            break;
+
+                        case ExifInterface.ORIENTATION_NORMAL:
+                        default:
+                            rotatedBitmap = bitmap;
+                    }
+
+                    imageView.setImageBitmap(rotatedBitmap);
+
+                    Bitmap scaled = Bitmap.createScaledBitmap(bitmap, classifier.getImageSizeX(),classifier.getImageSizeY(), false);
+                    classifyImage(scaled);
+
+                }catch(Exception e){
+                    System.err.println(e);
+                }
+            }else if (requestCode == PICK_IMAGE){
+                Uri uri = data.getData();
+
+                try {
+                    Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
+                    ImageView imageView = findViewById(R.id.imageView);
+                    imageView.setImageBitmap(bitmap);
+                    Bitmap scaled = Bitmap.createScaledBitmap(bitmap, classifier.getImageSizeX(),classifier.getImageSizeY(), false);
+                    classifyImage(scaled);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+
+            }
+        }
     }
     
 }
